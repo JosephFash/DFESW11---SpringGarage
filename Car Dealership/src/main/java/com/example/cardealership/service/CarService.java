8566012777f2dae8b34277a71364893b451e3dba
JosephFash <97948399+JosephFash@users.noreply.github.com>
2022-03-18 03:44:34 +0000

    Service File For Api

---
+++ Car Dealership/src/main/java/com/example/cardealership/service/CarService.java
@@ -0,0 +1,18 @@
+package com.example.cardealership.service;
+
+import com.example.cardealership.data.models.Car;
+import com.example.cardealership.data.payloads.request.CarRequest;
+import com.example.cardealership.data.payloads.response.MessageResponse;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public interface CarService {
+    MessageResponse createCar(CarRequest carRequest);
+    Optional<Car> updateCar(Integer CarId, CarRequest carRequest);
+    void deleteCar(Integer CarId);
+    Car getASingleCar(Integer CarId);
+    List<Car> getAllCar();
+}
